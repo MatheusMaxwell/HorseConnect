72ead5e99b2078f1e23cd8c08b05b812678b899b
Matheus Maxwell Meireles <matheusmeireles9@hotmail.com>
2022-11-10 09:25:17 -0300

    Initial Commit

---
+++ HorseConnect/HorseConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorseConnectApp.swift
+//  HorseConnect
+//
+//  Created by Matheus Maxwell Meireles on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct HorseConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
